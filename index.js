2bd1b05f4a5182e0fc4e294a3fb1e818d2cbcb94
Clemens Borys <cborys@akamai.com>
2020-12-07 11:51:47 +0100

    Add eslint

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-var socketIO = require("socket.io");
+const express = require('express');
+const app = express();
+const path = require('path');
+const socketIO = require('socket.io');
 
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, "index.html"));
+  response.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
-const server = app.listen(port , () => {
-    console.log("Hello, world!");
-    console.log(`Listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log('Hello, world!');
+  console.log(`Listening on port ${port}`);
 });
 
 
 const io = socketIO(server);
 
-io.on( "connection", (socket) => {
-    console.log("Client connected");
-    socket.on("disconnect", () => console.log("Client disconnected"));
+io.on( 'connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(
-    () => io.emit("time", new Date().toTimeString()),
-    1000
+    () => io.emit('time', new Date().toTimeString()),
+    1000,
 );
