be0f98f339234e819aa918a9653f461856358799
Clemens Borys <cborys@akamai.com>
2020-12-07 10:57:12 +0100

    Add a websocket connection that will console.log the current time

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var socketIO = require("socket.io");
 
 
-app.get('/', function (request, response) {
+app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, "index.html"));
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port , function() {
+const port = process.env.PORT || 8080;
+const server = app.listen(port , () => {
     console.log("Hello, world!");
+    console.log(`Listening on port ${port}`);
 });
+
+
+const io = socketIO(server);
+
+io.on( "connection", (socket) => {
+    console.log("Client connected");
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+setInterval(
+    () => io.emit("time", new Date().toTimeString()),
+    1000
+);
